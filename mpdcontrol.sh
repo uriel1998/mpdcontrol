632fc501e91b093cac67087ee96c35808f0c551b
Steven Saus <steven@stevesaus.com>
2026-05-19 11:11:33 -0400

    pseudocode

--- mpdcontrol.sh
+++ mpdcontrol.sh
@@ -9,7 +9,6 @@
 ################################################################################
 
 
-ADDMODE="1"    
 export SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 
 # Setting them here, will create if needed later
@@ -29,6 +28,19 @@ elif [ -f "$ConfigDir/mpdq.ini" ];then
 	"$ConfigDir/mpdq.ini"
 fi
 
+# check for helper programs
+grep_bin=$(which grep)
+mpc_bin=$(which mpc)
+fzf_bin=$(which fzf)
+mpdq_bin=$(which mpdq)
+jq_bin=$(which jq)
+
+
+ADDMODE="1"    
+host_arg=""
+LOUD="0"
+
+###############################################################functions
 
 function loud() {
     if [ $LOUD -eq 1 ];then
@@ -51,246 +63,73 @@ function read_variables() {
     else
         loud "Configuration file not found; using defaults."
     fi
+    
     # If there's no config file or a line is malformed or missing, sub in the default value
-
+    MPDBASE="$(echo "$config" | ${grep_bin} -e "^musicdir=" | cut -d = -f 2- ||
+        cat "$XDG_CONFIG_HOME/mpd/mpd.conf" | ${grep_bin} "^music" | cut -d'"' -f2 ||
+        echo $HOME/Music)"
     MPD_HOST="$(echo "$config" | ${grep_bin} -e "^mpdserver=" | cut -d = -f 2- || echo localhost)"
     MPD_PASS=$(echo "$config" | ${grep_bin} -e "^mpdpass=" | cut -d = -f 2-)
     MPD_PORT=$(echo "$config" | ${grep_bin} -e "^mpdport=" | cut -d = -f 2- || echo 6600)
     set_host_arg
-    # fallback of fallbacks
-	if [ -z "$MPD_HOST" ];then
-		MPD_HOST=localhost
-	fi
-    
+	DI_PLS_DIR="$(echo "$config" | ${grep_bin} -e "^DI_PLS_DIR=" | cut -d = -f 2- || echo "$DI_PLS_DIR")"
 }
+
+main (){
 
 
 
 # get playlists (prefix an emoji) 📋
+mpc --host "${host_arg}" --port "${MPD_PORT}" lsplaylists
 # get stations (prefix an emoji) 🎛️
-# get listen to di (prefix an emoji) 📡
-# get webradio presets from radiotray (prefix an emoji) 📻
-# get genre 🎼
-# get album_artist  🎸
-# get album 💿  (present as album by AlbumArtist)
-# get track 🎶 (present as track by Artist)
-mpc lsplaylists
 mpdq -e (need to trim path and ext)
-mpc list album group genre
-mpc list artist group genre 
-listen to di reads pls files
+# get listen to di or other playlist (prefix an emoji) 📡
+# Loop through all .pls files in the specified directory
+if [ -d "${DI_PLS_DIR}" ];then 
+	for pls_file in "${DI_PLS_DIR}"/*.pls; do
+		# Parse each .pls file
+		while IFS= read -r line; do
+			# Check if the line contains a File or Title
+			if [[ $line == File* ]]; then
+				url=$(echo "$line" | cut -d'=' -f2)
+			elif [[ $line == Title* ]]; then
+				title=$(echo "$line" | cut -d'=' -f2)
+				# Append title and url to variable 
+# TODO MAKE THE VARIABLE WITH STATION EMJOI				
+				echo "$title ‡ $url" >> "$temp_file"
+			fi
+		done < "$pls_file"
+	done
+# get webradio presets from radiotray (prefix an emoji) 📻
+if [ -d "${XDG_CONFIG_HOME}/radiotray-ng/bookmarks.json" ];then
+	${jq_bin} -r '
+		.[]
+		| .stations[]
+		| "\(.name) ‡ \(.url)"
+	' "${XDG_CONFIG_HOME}/radiotray-ng/bookmarks.json"
+# TODO MAKE THE VARIABLE WITH STATION EMJOI				
 
-all of these need to be stored in a single data format
-icon,source,title,full specification (url, text file to select on, whatever)
-then use case to split out how to pass them along
-	
-	(streamlink, url, whatever)
-	
-	Just do playlists and stations and listentodi to start, then add in the rest
+# get genre 🎼
+mpc --host "${host_arg}" --port "${MPD_PORT}" list album group genre
+# get album_artist  🎸
+mpc --host "${host_arg}" --port "${MPD_PORT}" list artist group genre 
+# get album 💿  (present as album by AlbumArtist)
+mpc --host "${host_arg}" --port "${MPD_PORT}" list album group genre 
+
+
+#all of these need to be stored in a single data format
+#icon,source,title,full specification (url, text file to select on, whatever)
 # present in big ass scrollable list
 # throw into fzf|rofi | php ?? 
-# get result, pipe back to mpc, mpdq, listen_to_di, however pipe in webradio preset
-  
 
-    
-    
-    if [ "$1" == "-c" ];then
-        ADDMODE="0"
-        shift
-    fi
-    
-now_album(){
-    
-    artist=$(mpc --host "$MPD_HOST" current --format "%artist%")
-    album=$(mpc --host "$MPD_HOST" current --format "%album%")
+#then use case to split out how to pass them along
+	
+#	(streamlink, url, whatever)
+	
 
-
-    if [[ -z "$artist" || -z "$album" ]]; then
-        echo "No song is currently playing."
-    else
-        clearmode
-        mpc --host "$MPD_HOST" search album "$album" | mpc add
-        mpc --host "$MPD_HOST" play
-    fi
-}
-    
-now_artist(){
-    
-    album_artist=$(mpc --host "$MPD_HOST" current --format "%albumartist%")
-    if [[ -z "$album_artist" ]]; then
-        echo "No song is currently playing or no album artist information available."
-    else
-        clearmode
-        mpc --host "$MPD_HOST" search albumartist "$album_artist" | mpc add
-        mpc --host "$MPD_HOST" play
-    fi
-
-}    
-    
-    
-    
-    
-
-clearmode (){  
-        if [ "$ADDMODE" = "0" ];then
-            mpc --host "$MPD_HOST" clear -q
-			# if there is anything in genre "Bumper" then choose one randomly and add it.
-			SongStem=$(mpc --host "${MPD_HOST}" find genre "Bumper" | shuf -n1 )
-			if [ "$SongStem" != "" ];then
-				# We will also use $SongFile as a continuing check
-				SongFile="$MPDBASE/$SongStem"
-				`mpc --host $MPD_HOST add "${SongStem}"`
-			fi            
-            # This doesn't work with the shuffle implementation and the way I add songs here.
-            #SongStem=$(mpc --host "${MPD_HOST}" -port "${MPD_PORT}" find genre "Bumper" | shuf -n1 )
-			#if [ "$SongStem" != "" ];then
-				#`mpc --host $MPD_HOST --port $MPD_PORT add "${SongStem}"`
-			#fi
-        fi
-}
-    
-    
-    
-interactive(){  
-
-    echo -e "\E[0;32m(\E[0;37mc\E[0;32m)ustom, \E[0;32m(\E[0;37mg\E[0;32m)enre, (\E[0;37mA\E[0;32m)lbumartist, (\E[0;37ma\E[0;32m)rtist, a(\E[0;37ml\E[0;32m)bum, (\E[0;37ms\E[0;32m)ong, (\E[0;37mp\E[0;32m)laylist, or (\E[0;37mq\E[0;32m)uit? "; tput sgr0
-    read -r CHOICE
-
-
-    case "$CHOICE" in
-        "c") 
-            if [ -f "$(which fzf)" ];then 
-                result=$(mpc --host "$MPD_HOST" list genre | fzf --multi)
-            else
-                result=$(mpc --host "$MPD_HOST" list genre | pick)
-            fi
-            
-            selection=""
-            while IFS= read -r genre; do
-                bob=$(mpc --host "$MPD_HOST" -f "%title% ‡ %artist%" search genre "$genre")
-                selection=$(echo "$selection $bob")
-            done <<< "$result"    
-            
-# for year in {1990..1999}; do     mpc -f "%album% - %artist% - %genre%" find genre "Rock" date "$year"; done  | sort | uniq                
-        ### TODO
-        ### THIS IS IT - WE CAN ACTUALLY PUT IN A SHORTCUT MATCH FOR OTHER FIELDS HERE TOO!
-        ### EG g:${genre} so if you want to limit what you're seeing, you can type g:Rock
-        ### I THINK if FZF does each term separately
-        # TODO - findadd BOTH artist and title, lololol
-            if [ -f "$(which fzf)" ];then 
-                result=$(echo "$selection" | fzf --multi)
-            else
-                result=$(echo "$selection" | pick )
-            fi            
-            clearmode
-            while IFS= read -r line; do
-                title=$(echo "${line}" | awk -F ' ‡' '{print $1}') 
-                artist=$(echo "${line}" | awk -F '‡ ' '{print $2}') 
-                echo "$title"
-                echo "$artist"
-                mpc --host "$MPD_HOST" findadd title "${title}" artist "${artist}"
-            done <<< "$result"
-            mpc --host "$MPD_HOST" play
-        ;;
-
-
-        "s") 
-            if [ -f "$(which fzf)" ];then 
-                result=$(mpc --host "$MPD_HOST" list title | fzf --multi)
-            else
-                result=$(mpc --host "$MPD_HOST" list title | pick)
-            fi            
-            clearmode
-            while IFS= read -r title; do
-                mpc --host "$MPD_HOST" findadd title "${title}" 
-            done <<< "$result"
-            mpc --host "$MPD_HOST" play
-        ;;
-
-        "A") 
-            if [ -f "$(which fzf)" ];then 
-                result=$(mpc --host "$MPD_HOST" list albumartist | fzf --multi)
-            else
-                result=$(mpc --host "$MPD_HOST" list albumartist | pick)
-            fi            
-            clearmode
-            while IFS= read -r albumartist; do
-                mpc --host "$MPD_HOST" findadd albumartist "${albumartist}" 
-                mpc --host "$MPD_HOST" shuffle -q
-                mpc --host "$MPD_HOST" play
-            done <<< "$result"
-        ;;
-        "a") 
-            if [ -f "$(which fzf)" ];then 
-                result=$(mpc --host "$MPD_HOST" list artist | fzf --multi)
-            else
-                result=$(mpc --host "$MPD_HOST" list artist | pick)
-            fi
-            clearmode
-            while IFS= read -r artist; do
-                mpc --host "$MPD_HOST" findadd artist "${artist}" 
-                mpc --host "$MPD_HOST" shuffle -q
-                mpc --host "$MPD_HOST" play
-            done <<< "$result"
-        ;;
-        "l") 
-
-            if [ -f "$(which fzf)" ];then 
-                result=$(mpc --host "$MPD_HOST" list album | fzf --multi)
-            else
-                result=$(mpc --host "$MPD_HOST" list album | pick)
-            fi
-            clearmode
-            while IFS= read -r album; do
-                mpc --host "$MPD_HOST" findadd album "$album"
-                mpc --host "$MPD_HOST" random off
-                mpc --host "$MPD_HOST" play
-            done <<< "$result"
-        ;;
-
-        "g") 
-            if [ -f "$(which fzf)" ];then 
-                result=$(mpc --host "$MPD_HOST" list genre | fzf --multi)
-            else
-                result=$(mpc --host "$MPD_HOST" list genre | pick)
-            fi
-            clearmode
-            while IFS= read -r genre; do
-                mpc --host "$MPD_HOST" findadd genre "$genre" 
-                mpc --host "$MPD_HOST" shuffle -q
-                mpc --host "$MPD_HOST" play
-            done <<< "$result"
-        ;;
-        "p")
-            if [ -f "$(which fzf)" ];then 
-                result=$(mpc --host "$MPD_HOST" lsplaylists | fzf --multi)
-            else
-                result=$(mpc --host "$MPD_HOST" lsplaylists | pick)
-            fi
-            clearmode            
-            while IFS= read -r playlist; do
-                mpc --host "$MPD_HOST" load "$playlist" 
-                mpc --host "$MPD_HOST" play
-            done <<< "$result"
-        ;;
-        "q")
-        ;;
-        "h") echo "Use -c to clear before adding.  Export your MPD_HOST as PASS@HOST; localhost is default";;
-        *)            echo "You have chosen poorly. Run without commandline input.";;
-    esac
 }
 
+##############################################################entrypoint
 
-case "${1}" in 
-    nowal*|now_al*)
-                now_album
-                ;;
-    nowar*|now_ar*)
-                now_artist
-                ;;
-    -c) ADDMODE="0"
-        shift
-        ;;
-    *) interactive "${@}"
-        ;;
-esac
+read_variables
+main "${@}"
